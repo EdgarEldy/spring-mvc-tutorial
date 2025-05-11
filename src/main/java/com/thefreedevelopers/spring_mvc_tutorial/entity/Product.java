45e2613c9ffa2f254f306e4562d4aeb7c52a9488
EdgarEldy <ed.eldy21@live.com>
2025-05-11 11:01:04 +0200

    Rearrange code

--- src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Product.java
+++ src/main/java/com/thefreedevelopers/spring_mvc_tutorial/entity/Product.java
@@ -13,6 +13,14 @@ public class Product {
     private Long id;
     private String productName;
     private float unitPrice;
+    //Setting ManyToOne relationship to Category
+    @ManyToOne
+    @JoinColumn(name="categoryId", nullable = false)
+    private Category category;
+
+    //Constructor
+    public Product() {
+    }
 
     public String getProductName() {
         return productName;
@@ -30,16 +38,12 @@ public class Product {
         this.unitPrice = unitPrice;
     }
 
-    //Constructor
-    public Product() {
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 
     public Category getCategory() {
