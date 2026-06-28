506fc7e9c69b7c332e3316957f7edd95d71296ad
EdgarEldy <ed.eldy21@live.com>
2026-06-28 21:55:58 +0400

    refactor: rename package from com.thefreedevelopers to com.edgareldy
    - Move source files to src/main/java/com/edgareldy/spring_mvc_tutorial/
    - Move test files to src/test/java/com/edgareldy/spring_mvc_tutorial/
    - Update package declarations in SpringMvcTutorialApplication and tests
    - Remove old com.thefreedevelopers directory tree

--- src/test/java/com/edgareldy/spring_mvc_tutorial/SpringMvcTutorialApplicationTests.java
+++ src/test/java/com/edgareldy/spring_mvc_tutorial/SpringMvcTutorialApplicationTests.java
@@ -1,4 +1,4 @@
-package com.thefreedevelopers.spring_mvc_tutorial;
+package com.edgareldy.spring_mvc_tutorial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
