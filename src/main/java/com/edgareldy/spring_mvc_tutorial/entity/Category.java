8371c1e190db3ade87e998265b588859ba6f0069
EdgarEldy <ed.eldy21@live.com>
2026-06-28 22:09:12 +0400

    refactor: migrate all sources from com.thefreedevelopers to com.edgareldy package
    - Move all Java files to src/main/java/com/edgareldy/spring_mvc_tutorial/
    - Update all package declarations and imports
    - Add missing parameterized constructors to Category and Product entities
    - Remove duplicate Customer() empty constructor

---
+++ src/main/java/com/edgareldy/spring_mvc_tutorial/entity/Category.java
@@ -0,0 +1,36 @@
+package com.edgareldy.spring_mvc_tutorial.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String categoryName;
+    //Setting OneToMany relationship to Product
+    @OneToMany(mappedBy = "category")
+    private Set<Product> products;
+
+    //Constructor
+    public Category() {
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/edgareldy/spring_mvc_tutorial/entity/Customer.java
@@ -0,0 +1,78 @@
+package com.edgareldy.spring_mvc_tutorial.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    // Properties
+    private String firstName;
+    private String lastName;
+    private String Tel;
+    private String Email;
+    private String Address;
+    // Constructor
+    public Customer() {
+    }
+
+    // Constructor with initial values
+    public Customer(String firstName, String lastName, String tel, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.Tel = tel;
+        this.Email = email;
+        this.Address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String tel) {
+        Tel = tel;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
